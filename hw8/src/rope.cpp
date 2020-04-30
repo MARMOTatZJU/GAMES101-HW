e300d1b35f821ac1839dc2119d5410e2e6125d31
xuyinda <ydxu_zju@hotmail.com>
2020-04-30 18:20:12 +0800

    hw8: rope constructor

--- hw8/src/rope.cpp
+++ hw8/src/rope.cpp
@@ -12,11 +12,38 @@ namespace CGL {
     Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
     {
         // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+        
+        Vector2D step_vec = (end - start) / (num_nodes - 1.0);
+        
+        Vector2D current_node=start;
+        Vector2D previous_node; 
+        Mass* current_mass = new Mass(current_node, node_mass, false);
+        Mass* previous_mass = nullptr;
+        masses.push_back(current_mass);
+        Spring* current_spring = nullptr;
+        for (auto ith=1;ith<=num_nodes;++ith)
+        {
+            // std::clog << num_nodes << std::endl;
+            if (ith != num_nodes)
+            {
+                current_node = start + ith*step_vec;
+            }
+            else
+            {
+                current_node = end;
+            }
+            previous_mass = current_mass;
+            current_mass = new Mass(current_node, node_mass, false);
+            current_spring = new Spring(previous_mass, current_mass, k);
+            // append current mass and spring
+            masses.push_back(current_mass);
+            springs.push_back(current_spring);
+        }
 
 //        Comment-in this part when you implement the constructor
-//        for (auto &i : pinned_nodes) {
-//            masses[i]->pinned = true;
-//        }
+       for (auto &i : pinned_nodes) {
+           masses[i]->pinned = true;
+       }
     }
 
     void Rope::simulateEuler(float delta_t, Vector2D gravity)
