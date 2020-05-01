8c49fb741eb81a273a309abe1d672318aceefe0f
xuyinda <ydxu_zju@hotmail.com>
2020-05-01 13:06:12 +0800

    hw8: add verlet

--- hw8/README.md
+++ hw8/README.md
@@ -1,6 +1,5 @@
-
 Legenda:
-- blue: explicit euler
+- blue: explicit euler (may be overlapped with red rope)
 - red: semi implicit euler
 - green: verlet
 
@@ -11,9 +10,20 @@ Completed tasks:
     - diverge after several minutes
   - Semi Implicit Euler simulation (red)
     - do not diverge, but will not stop trembling
-- 
+  - global damping
+  - internal damping
+- verlet method
+  - gloal damping
 
+Observation:
+- internal damping works excelently to stabilize euler simulation
+  - even explicit euler will be stabilized after introducing internal damping.
+  - however, internal damping will no stop rope from oscillating (thus global damping is needed)
+- under settings with both internal damping and global damping, explicit euler will get very close simulation result with semi implicit euler
+  - tow ropes are totally overlapped
 
 P.S.
 - The formula for explicit Euler method seems to have a wrong sign. The substraction sign may be needed to removed? 
   - After removing it, my simulation is able to run normaly.
+- A red rope has been added to simulate semi implicit euler and compare it with explicit euler.
+  - void simulateSemiImplicitEuler(...);
--- hw8/src/rope.cpp
+++ hw8/src/rope.cpp
@@ -52,9 +52,16 @@ namespace CGL {
             // TODO (Part 2): Use Hooke's law to calculate the force on a node
             auto vec_12 = s->m2->position - s->m1->position;
             auto vec_12_norm = vec_12.norm();
-            Vector2D f = s->k * vec_12 / vec_12_norm * (vec_12_norm - s->rest_length);
+            auto vec_12_unit = vec_12 / vec_12_norm;
+            Vector2D f = s->k * vec_12_unit * (vec_12_norm - s->rest_length);
             s->m1->forces += f;
             s->m2->forces += -f;
+            // add internal damping force
+            float k_d = 500;
+            Vector2D velocity_ba = s->m2->velocity - s->m1->velocity;
+            Vector2D f_d = k_d * dot(vec_12_unit, velocity_ba) * vec_12_unit;
+            s->m1->forces += f_d;
+            s->m2->forces += -f_d;
         }
         for (auto &m : masses)
         {
@@ -62,11 +69,14 @@ namespace CGL {
             {
                 // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
                 m->forces += gravity * m->mass;
+                // TODO (Part 2): Add global damping
+                float k_d_global = 0.01;
+                m->forces += - k_d_global * m->velocity;
+                // resultant acceleration
                 Vector2D a = m->forces / m->mass;
                 // explicit euler
                 m->position += m->velocity * delta_t;
                 m->velocity += a * delta_t;
-                // TODO (Part 2): Add global damping
             }
 
             // Reset all forces on each mass
@@ -82,21 +92,32 @@ namespace CGL {
             // TODO (Part 2): Use Hooke's law to calculate the force on a node
             auto vec_12 = s->m2->position - s->m1->position;
             auto vec_12_norm = vec_12.norm();
-            Vector2D f = s->k * vec_12 / vec_12_norm * (vec_12_norm - s->rest_length);
+            auto vec_12_unit = vec_12 / vec_12_norm;
+            Vector2D f = s->k * vec_12_unit * (vec_12_norm - s->rest_length);
             s->m1->forces += f;
             s->m2->forces += -f;
+            // add internal damping force
+            float k_d = 500;
+            Vector2D velocity_ba = s->m2->velocity - s->m1->velocity;
+            Vector2D f_d = k_d * dot(vec_12_unit, velocity_ba) * vec_12_unit;
+            s->m1->forces += f_d;
+            s->m2->forces += -f_d;
         }
         for (auto &m : masses)
         {
             if (!m->pinned)
             {
                 // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
+                // add gravidy
                 m->forces += gravity * m->mass;
+                // TODO (Part 2): Add global damping
+                float k_d_global = 0.01;
+                m->forces += - k_d_global * m->velocity;
+                // resultant acceleration 
                 Vector2D a = m->forces / m->mass;
                 // semi implicit euler
                 m->velocity += a * delta_t;
                 m->position += m->velocity * delta_t;
-                // TODO (Part 2): Add global damping
             }
 
             // Reset all forces on each mass
@@ -109,6 +130,11 @@ namespace CGL {
         for (auto &s : springs)
         {
             // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
+            auto vec_12 = s->m2->position - s->m1->position;
+            auto vec_12_norm = vec_12.norm();
+            Vector2D f = s->k * vec_12 / vec_12_norm * (vec_12_norm - s->rest_length);
+            s->m1->forces += f;
+            s->m2->forces += -f;
         }
 
         for (auto &m : masses)
@@ -117,9 +143,16 @@ namespace CGL {
             {
                 Vector2D temp_position = m->position;
                 // TODO (Part 3.1): Set the new position of the rope mass
-                
+                m->forces += gravity * m->mass;
+                Vector2D a = m->forces / m->mass;
                 // TODO (Part 4): Add global Verlet damping
+                float damping_factor = 0.000002;
+                m->position = m->position + (1 - damping_factor)*(m->position - m->last_position) + a*delta_t*delta_t;
+                // update last_position
+                m->last_position = temp_position;
             }
+            // Reset all forces on each mass
+            m->forces = Vector2D(0, 0);
         }
     }
 }
